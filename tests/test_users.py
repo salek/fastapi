35e5e3e5e0eeb0fd02e4887201181ffd6c30cbea
salek <nuraliyev.salamat@gmail.com>
2021-12-29 18:18:03 +0500

    creating tests

---
+++ tests/test_users.py
@@ -0,0 +1,22 @@
+from app import schemas
+from .database import client, session
+
+
+# def test_root(client):
+#     res = client.get("/")
+#     print(res.json())
+#     assert res.json().get('message') == 'Welcome!'
+#     assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+
+def test_login_user(client):
+    res = client.post("/login", data={"username": "hello123@gmail.com", "password": "password123"})
+    print(res.json())
+    assert res.status_code == 200
